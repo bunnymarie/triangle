97257ec9d182ea7a932e12841b2270e1f9989212
Derek Hammer, James Hanley and Marie Dillard <derek@>
2018-05-31 16:07:43 -0700

    Add formula for triangles base page finish.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $("form#triangle").submit(function(event){
+      event.preventDefault();
+    var one = parseInt($("#sideone").val());
+    var two = parseInt($("#sidetwo").val());
+    var three = parseInt($("#sidethree").val());
+    debugger;
+    if (one >= 180 || one <= 0 || two >= 180 || two >= 0 || three >= 180 || three >= 0 ){
+      alert("No")
+    } else if (one === two && one === three){
+      alert("Equal");
+    } else if (one === 0 || two == 0 || three === 0){
+      alert("not a triangle");
+    }
+    else if (one === two || one === three || three === two){
+      alert("Isc");
+    } else if (!one === two && !one === three && !three === two ||  one%two === 0 || one%three === 0 || two%three === 0 || two%one === 0 || three%one === 0 || three%two === 0){
+      alert("scalene");
+    }
+    else {
+    alert("not a triangle");
+    }
+  });
+});
